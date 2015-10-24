715006f4ce1c10cfccbf60b3beff9efbebaf6250
David <ddizoya@danielcastelao.org>
2015-10-25 01:27:38 +0200

    ;D

--- src/pkg/XMLproba0.java
+++ src/pkg/XMLproba0.java
@@ -1,7 +1,11 @@
 package pkg;
 
+import java.awt.Desktop;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URI;
+import java.util.concurrent.BrokenBarrierException;
 
 import javax.xml.namespace.NamespaceContext;
 import javax.xml.stream.XMLOutputFactory;
@@ -11,7 +15,7 @@ import javax.xml.stream.XMLStreamWriter;
 public class XMLproba0 {
 	private XMLStreamWriter xsw = null;
 	private XMLOutputFactory xof = XMLOutputFactory.newInstance();
-	private String fich = "C:\\Users\\David\\Desktop\\autores.xml";
+	public String fich = "C:\\Users\\David\\Desktop\\autores.xml";
 
 	private String autor1[] = { "a1", "Alexandre Dumas", "El conde de Montecristo", "Los miserables" };
 	private String autor2[] = { "a2", "Fiodor Dostoyevski", "El idiota", "Noches blancas" };
@@ -52,7 +56,11 @@ public class XMLproba0 {
 		XMLproba0 obj = new XMLproba0 ();
 		try {
 			obj.crearXML();
-		} catch (XMLStreamException | IOException e) {
+			
+			Desktop d = Desktop.getDesktop();	
+			d.open(new File(obj.fich));
+			
+			} catch (XMLStreamException | IOException e) {
 			e.printStackTrace();
 		}
 	}
